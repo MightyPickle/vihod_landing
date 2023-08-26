bed053d6f65c417b49810fef36e1a9ca1993f3dd
Aleksandra Bychkova <abychkova2@rencredit.ru>
2023-08-26 17:33:20 +0300

    Поправила всю дату на портретную дату

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,4 +1,15 @@
 // @ts-ignore
 import data from './answers.json';
+import { UserPortraitQuestions } from './enums/enumQuestionsPortrait';
 
 export const allData = data;
+
+const enumValues = Object.values(UserPortraitQuestions);
+
+export const portraitData = allData.map((obj) => Object.entries(obj).reduce((filteredObj, [key, value]) => {
+  if (enumValues.includes(key)) {
+    // eslint-disable-next-line no-param-reassign
+    filteredObj[key] = value;
+  }
+  return filteredObj;
+}, {}));
