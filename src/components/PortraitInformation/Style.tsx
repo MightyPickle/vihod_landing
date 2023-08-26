99c5aea3adfc3fefee3e029235c0da27d029c7fc
Aleksandra Bychkova <abychkova2@rencredit.ru>
2023-08-26 21:45:33 +0300

    Поправила использование styled components

--- src/components/PortraitInformation/Style.tsx
+++ src/components/PortraitInformation/Style.tsx
@@ -1,14 +1,9 @@
 import { styled } from 'styled-components';
-import React from 'react';
 
-const FlexSection = styled.div`
+export const FlexSection = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
 align-content: center;
 margin: 20px;
 `;
-
-export function FlexWrapper({ props }) {
-  return <FlexSection>{props}</FlexSection>;
-}
