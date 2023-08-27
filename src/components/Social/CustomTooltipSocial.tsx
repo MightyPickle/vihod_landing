1534cb95511b1fbace448f8877c4642a2b390b6d
Yulia Ten <uliaten10@gmail.com>
2023-08-27 13:35:44 +0300

    add colors

--- src/components/Social/CustomTooltipSocial.tsx
+++ src/components/Social/CustomTooltipSocial.tsx
@@ -3,16 +3,18 @@ import { CustomTooltipWrapper } from '../rechartComponents/styledTooltipWrapper'
 
 function CustomTooltipSocial({ active, payload, label }) {
   if (active) {
+    console.log(payload);
+
     return (
       <CustomTooltipWrapper>
-        <p>
+        <p style={{ color: payload[0].stroke }}>
           {payload[0].name}
           :
           {' '}
           {payload[0].payload.cisAmount}
 
         </p>
-        <p>
+        <p style={{ color: payload[1].stroke }}>
           {payload[1].name}
           :
           {' '}
